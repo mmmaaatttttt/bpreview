4b29b3937d801b4f7917c30e326ebfd1a5fd6daf
Matt Lane <lane.matthew@gmail.com>
2015-03-11 22:13:36 -0500

    token setup

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 
     def signup_email(user)
         @user = user
-        @twitter_message = "#Shaving is evolving. Excited for @harrys to launch."
+        @twitter_message = "#StartCommonCore with the Common Core Unit Blueprints. Excited for this project to launch!"
 
         mail(:to => user.email, :subject => "Thanks for signing up!")
     end
